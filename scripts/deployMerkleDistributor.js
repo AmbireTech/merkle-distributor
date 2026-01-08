f0a7ced9b6e4fa3866d1ef18ece20ebc97facbf7
stojnovsky <stojnovsky@gmail.com>
2026-01-08 16:57:15 +0200

    changed claim method to safeTransferFrom

--- scripts/deployMerkleDistributor.js
+++ scripts/deployMerkleDistributor.js
@@ -4,10 +4,10 @@ const { ethers } = require('hardhat')
 
 async function main() {
   const MerkleDistributor = await ethers.getContractFactory('MerkleDistributor')
-  const merkleDistributor = await MerkleDistributor.deploy(
-    // USDC
-    '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
-    '0x525df38ec587d18df62b7e004e44c6cb4c3af6981d1d3f156e73eeafb1a128a5'
+  const merkleDistributor = await MerkleDistributor.deploy( 
+    '0xE575cC6EC0B5d176127ac61aD2D3d9d19d1aa4a0', // Token
+    '0xbe154afea9ba1e08729654a19c53952a892d6b37fe0b5d1bdf8ac4f51d03a426', // MerkleRoot
+    '0xFDE6d7303868fD2046c15263C9268618092664d1' //vault (buybacks.ambire.eth)
   )
   await merkleDistributor.deployed()
   console.log(`merkleDistributor deployed at ${merkleDistributor.address}`)
